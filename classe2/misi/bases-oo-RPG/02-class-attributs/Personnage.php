93768d34d393ed007b1ad972bdb80cc4cfcfa9dc
Daniel Misi <misi@MacBook-Air-de-Daniel.local>
2025-08-29 15:40:34 +0200

    02-class-attributs

--- classe2/misi/bases-oo-RPG/02-class-attributs/Personnage.php
+++ classe2/misi/bases-oo-RPG/02-class-attributs/Personnage.php
@@ -5,6 +5,10 @@
 class Personnage
 {
     // Attributs
+    public string $genre; // attribut public sans valeurs
+    public int $age = 25; // avec valeur par défaut
+    public null|string|int $mois; // 3 types possibles
+
     // Constantes
     // Méthodes
 }
